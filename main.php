7f39ba79e58489e0a766716123766ae1c1443871
Charles Patterson <charlesspatterson@gmail.com>
2014-11-17 21:14:32 -0500

    main
    main page

---
+++ main.php
@@ -0,0 +1,105 @@
+<?php
+
+include 'common/header.php';
+include 'common/navbar.php';
+
+?>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <div class="container" align="center">
+            <h1 style="font-weight: bold; font-size: 15vh">APOLLO</h1>
+            <p>The all-in-one personal service that reminds you of the media you really want</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Get Started &raquo;</a></p>
+        </div>
+    </div>
+
+    <!-- Row 1 -->
+    <div class="container">
+        <div class="row" align="center">
+            <div class="col-md-6">
+                <h2>What We Do</h2>
+                <p align="justify">Have you ever seen a trailer for a movie you REALLY wanted to see, but never got around to it? Did you know you weren't going to see it before the advertising for it had stopped? Sometimes we just miss things, and nobody has the time to write one million sticky notes. Apollo is here to make sure that you never miss or forget about another great piece of media by sending you a customized email notification every once in a while of all the cool stuff you wanted to check out but didn't.</p>
+            </div>
+            <div class="col-md-6">
+                <h2>How It Works</h2>
+                <table style="padding: 25px 50px;">
+                    <tr>
+                        <td><i class="fa fa-sign-in fa-2x"></i></td>
+                        <td>&nbsp&nbsp&nbspSign in (or sign up)</td>
+                    </tr>
+                    <tr>
+                        <td> <i class="fa fa-star-o fa-2x"></i></td>
+                        <td>&nbsp&nbsp&nbspPick your favorites</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-envelope-o fa-2x"></i></td>
+                        <td>&nbsp&nbsp&nbspGet your notification</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp<i class="fa fa-child fa-2x"></i></td>
+                        <td>&nbsp&nbsp&nbspBuy your stuff</td>
+                    </tr>
+                </table>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <!-- Row 2 -->
+    <div style="background-color:#eee">
+        <div class="container" >
+            <div class="row" align="center">
+                <h2 style ="font-weight: bold">What We Cover</h2>
+                <div class="col-md-4">
+                    <h3>Music</h3>
+                    <p><i class="fa fa-music fa-5x"></i></p>
+                    <p>Check out the latest albums<br>from the hottest new artists!</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Movies</h3>
+                    <p><i class="fa fa-film fa-5x"></i></p>
+                    <p>From action movies to romantic comedies,<br>find the latest here (and in a theater near you)</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Games</h3>
+                    <p><i class="fa fa-gamepad fa-5x"></i></p>
+                    <p>Call of Duty, Halo, FIFA, whatever.<br>Don't miss the games you want.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <!-- Row 3 -->
+    <div style="padding-bottom: 10px">
+        <div class="container">
+            <div class="row" align="center">
+                <h2 style ="font-weight: bold">Why Use Apollo?</h2>
+                <div class="col-md-4" align="center">
+                    <h2>Free</h2>
+                    <p><i class="fa fa-money fa-5x"></i></p>
+                    <p>There are hundreds of ways to say "money".<br>Call it what you want, but keep it in your pockets.</p>
+                </div>
+                <div class="col-md-4" align="center">
+                    <h2>Personal</h2>
+                    <p><i class="fa fa-smile-o fa-5x"></i></p>
+                    <p>We'll show you the things you DO like and things<br>you WILL like, not stuff you MIGHT like.</p>
+                </div>
+                <div class="col-md-4" align="center">
+                    <h2>Convenient</h2>
+                    <p><i class="fa fa-check fa-5x"></i></p>
+                    <p>You could check ten different places, but life is just<br>easier when everything you need is all in one place.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+
+include 'common/footer.php';
+
+?>
